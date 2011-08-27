28a45ede6f69065fe0cc9b85d1557bc9d273059c
David Wolf <dpwolf@mac.com>
2011-08-27 17:48:19 -0400

    added socket.io hello world

--- app.js
+++ app.js
@@ -1,46 +1,62 @@
+// 
+// /**
+//  * Module dependencies.
+//  */
+// 
+// var express = require('express');
+// 
+// var app = module.exports = express.createServer();
+// 
+// // Configuration
+// 
+// app.configure(function(){
+//   app.set('views', __dirname + '/views');
+//   app.set('view engine', 'jade');
+//   app.use(express.bodyParser());
+//   app.use(express.methodOverride());
+//   app.use(app.router);
+//   app.use(express.static(__dirname + '/public'));
+// });
+// 
+// app.configure('development', function(){
+//   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+// });
+// 
+// app.configure('production', function(){
+//   app.use(express.errorHandler()); 
+// });
+// 
+// // Routes
+// 
+// app.get('/', function(req, res){
+//   res.render('index', {
+//     title: 'Express'
+//   });
+// });
+// 
+// app.listen(process.env.NODE_ENV === 'production' ? 80 : 8000, function() {
+//   console.log('Ready');
+// 
+//   // if run as root, downgrade to the owner of this file
+//   if (process.getuid() === 0)
+//     require('fs').stat(__filename, function(err, stats) {
+//       if (err) return console.log(err)
+//       process.setuid(stats.uid);
+//     });
+// });
 
-/**
- * Module dependencies.
- */
+var app = require('express').createServer()
+  , io = require('socket.io').listen(app);
 
-var express = require('express');
+app.listen(80);
 
-var app = module.exports = express.createServer();
-
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
-
-// Routes
-
-app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
   });
-});
-
-app.listen(process.env.NODE_ENV === 'production' ? 80 : 8000, function() {
-  console.log('Ready');
-
-  // if run as root, downgrade to the owner of this file
-  if (process.getuid() === 0)
-    require('fs').stat(__filename, function(err, stats) {
-      if (err) return console.log(err)
-      process.setuid(stats.uid);
-    });
 });
